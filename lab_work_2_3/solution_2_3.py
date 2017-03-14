a9ce169884156cd0bf14fcfedcdfab4c9c193b70
demydovb <demydovb@gmail.com>
2017-03-14 20:29:06 +0200

    Lab Work 2.3. added

---
+++ lab_work_2_3/solution_2_3.py
@@ -0,0 +1,47 @@
+"""
+Generate string with lowercase and uppercase alphabet plus numbers
+"""
+
+string = 'HelloUsername1234'
+
+"""
+Print 1st symbol of string
+"""
+
+print(string[0])
+
+"""
+Print last symbol
+"""
+
+print(string[-1])
+
+"""
+Print 3rd from start and 3rd from the end
+"""
+
+print(string[2:-2])
+
+"""
+Slice first 8 symbols
+"""
+
+print(string[0:8])
+
+
+"""
+Print only symbols with index, which divides on 3 without remaining
+"""
+
+print(string[3:len(string):3])
+
+"""
+Print the symbol of the middle of the string text
+"""
+print(string[len(string)//2])
+
+"""
+Reverse text using slice
+"""
+
+print(string[::-1])
